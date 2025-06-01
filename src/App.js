fe28931ea0135ebc3be6c45cc77483f53caa7321
bassinebrabije <brabije.yassine123@gmail.com>
2025-06-01 01:00:41 +0100

    analiL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Nav from './components/Nav';
-import { Analytics } from '@vercel/analytics/react';
+
 
 
 function App() {
   return (
     <div className='bg-[#000]'>
       <Nav />
-      <Analytics />
+
     </div>
   );
 }
